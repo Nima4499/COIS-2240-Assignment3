ae55e70af2bcca713657c1d6cbbebcaa411d150d
Nima4499 <nimafarzandi@gmail.com>
2024-12-05 21:36:43 -0500

    added before

--- LibraryManagementTest.java
+++ LibraryManagementTest.java
@@ -11,3 +11,19 @@ public class LibraryManagementTest {
     private Member member1;
     private Library library;
     private Transaction transaction;
+
+@Before
+    public void setUp() {
+        
+        library = new Library();
+        try {
+            book1 = new Book(100, "Book 1"); 
+            book2 = new Book(200, "Book 2"); 
+            library.addBook(book1);
+            library.addBook(book2);
+        } catch (Exception e) {
+            fail("Error while initializing books" + e.getMessage());
+        }
+        member1 = new Member(1, "Anna Green");
+        transaction = Transaction.getTransaction();
+    }
