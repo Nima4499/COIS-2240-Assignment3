5c0c81764d9d359b6730fa65b35d5f5205284d79
Nima4499 <nimafarzandi@gmail.com>
2024-12-05 21:36:03 -0500

    added variables

--- LibraryManagementTest.java
+++ LibraryManagementTest.java
@@ -3,3 +3,11 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
+
+public class LibraryManagementTest {
+    
+    private Book book1;
+    private Book book2;
+    private Member member1;
+    private Library library;
+    private Transaction transaction;
